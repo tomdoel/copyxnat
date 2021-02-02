452b1f60b74a9db30b4639ac2cdca0e62530ff98
Tom Doel <tomdoel@users.noreply.github.com>
2021-02-02 12:04:17 +0000

    refactor outputs

--- copyxnat/xnat/commands.py
+++ copyxnat/xnat/commands.py
@@ -19,7 +19,7 @@ class Command:
     """Wraps a command function and its input variables"""
     def __init__(self, command_inputs, initial_result=None):
         self.initial_result = initial_result
-        self.output_result = initial_result
+        self.outputs = initial_result
         self.output_tostring = self.result_tostring
         self.inputs = {
             'dst_xnat': command_inputs.dst_xnat,
@@ -28,26 +28,24 @@ class Command:
             'reporter': command_inputs.reporter
         }
 
-    @staticmethod
     @abc.abstractmethod
-    def run(xnat_item, inputs, outputs, from_parent):
+    def run(self, xnat_item, inputs, outputs, from_parent):
         """Function that will be run on each item in the XNAT server"""
 
     def run_command(self, xnat_item, from_parent):
         command_return = self.run(xnat_item=xnat_item,
                                   inputs=self.inputs,
-                                  outputs=self.output_result,
+                                  outputs=self.outputs,
                                   from_parent=from_parent)
-        self.output_result = command_return.outputs
         if command_return.tostring:
             self.output_tostring = command_return.tostring
         return command_return
 
     def get_outputs(self):
-        return {'result': self.output_result, 'tostring': self.output_tostring}
+        return {'result': self.outputs, 'tostring': self.output_tostring}
 
     def reset(self):
-        self.output_result = None
+        self.outputs = None
 
     def result_tostring(self, result):
         return "Output from {}: {}".format(self.NAME, result)
@@ -63,8 +61,7 @@ class CommandInputs:
 
 class CommandReturn:
     """Class for return values for a command function call"""
-    def __init__(self, outputs, to_children=None, recurse=True, tostring=None):
-        self.outputs = outputs
+    def __init__(self, to_children=None, recurse=True, tostring=None):
         self.to_children = to_children
         self.recurse = recurse
         self.tostring = tostring
@@ -75,11 +72,10 @@ class ExportCommand(Command):
     USE_DST_SERVER = False
     CACHE_TYPE = 'downloads'
 
-    @staticmethod
-    def run(xnat_item, inputs, outputs, from_parent):  # pylint: disable=unused-argument
+    def run(self, xnat_item, inputs, outputs, from_parent):  # pylint: disable=unused-argument
         """Command which exports XNAT project data onto local disk"""
         return_value = xnat_item.export()
-        return CommandReturn(outputs=outputs, to_children=return_value)
+        return CommandReturn(to_children=return_value)
 
 
 class ShowCommand(Command):
@@ -87,11 +83,10 @@ class ShowCommand(Command):
     USE_DST_SERVER = False
     CACHE_TYPE = 'cache'
 
-    @staticmethod
-    def run(xnat_item, inputs, outputs, from_parent):  # pylint: disable=unused-argument
+    def run(self, xnat_item, inputs, outputs, from_parent):  # pylint: disable=unused-argument
         """Command which displays contents of XNAT projects"""
         print(xnat_item.user_visible_info())
-        return CommandReturn(outputs=outputs)
+        return CommandReturn()
 
 
 class CopyCommand(Command):
@@ -99,8 +94,7 @@ class CopyCommand(Command):
     USE_DST_SERVER = True
     CACHE_TYPE = 'cache'
 
-    @staticmethod
-    def run(xnat_item, inputs, outputs, from_parent):
+    def run(self, xnat_item, inputs, outputs, from_parent):
         """Command which copies XNAT projects between servers"""
 
         # Override the project name
@@ -112,7 +106,7 @@ class CopyCommand(Command):
             fix_scan_types=inputs['fix_scan_types'],
             dst_label=dst_name,
             dry_run=inputs['reporter'].dry_run)
-        return CommandReturn(outputs, to_children=copied_item)
+        return CommandReturn(to_children=copied_item)
 
 
 class CheckDatatypesCommand(Command):
@@ -120,16 +114,15 @@ class CheckDatatypesCommand(Command):
     USE_DST_SERVER = True
     CACHE_TYPE = 'cache'
 
-    @staticmethod
-    def run(xnat_item, inputs, outputs, from_parent):  # pylint: disable=unused-argument
+    def run(self, xnat_item, inputs, outputs, from_parent):  # pylint: disable=unused-argument
         """
         Command which checks if destination server has all the
         scan datatypes that are present on the source server
         """
 
         # On first run, set up the outputs
-        if outputs is None:
-            outputs = {
+        if self.outputs is None:
+            self.outputs = {
                 'datatypes_on_dest': inputs['dst_xnat'].datatypes(),
                 'missing_session_datatypes': set(),
                 'ids_with_empty_datatypes': set(),
@@ -138,10 +131,10 @@ class CheckDatatypesCommand(Command):
 
         datatype = xnat_item.interface.datatype()
         if isinstance(xnat_item, XnatExperiment):
-            outputs['required_experiment_datatypes'].add(datatype)
+            self.outputs['required_experiment_datatypes'].add(datatype)
 
-            if datatype not in outputs['datatypes_on_dest']:
-                outputs['missing_session_datatypes'].add(datatype)
+            if datatype not in self.outputs['datatypes_on_dest']:
+                self.outputs['missing_session_datatypes'].add(datatype)
                 inputs['reporter'].warning(
                     'Session datatype {} needs to be added to '
                     'destination server. '.format(datatype))
@@ -161,9 +154,9 @@ class CheckDatatypesCommand(Command):
                                         xnat_item.full_name)
 
             inputs['reporter'].warning('Empty datatype for {}'.format(item_id))
-            outputs['ids_with_empty_datatypes'].add(item_id)
+            self.outputs['ids_with_empty_datatypes'].add(item_id)
 
-        return CommandReturn(outputs=outputs, tostring=check_datatypes_tostring)
+        return CommandReturn(tostring=check_datatypes_tostring)
 
 
 def check_datatypes_tostring(result):
