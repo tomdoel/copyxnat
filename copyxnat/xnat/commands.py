ac8c372c14eaa989aea9c26fd7130535cb66850b
Tom Doel <tomdoel@users.noreply.github.com>
2021-02-02 13:29:21 +0000

    Improve output from check_datatypes

--- copyxnat/xnat/commands.py
+++ copyxnat/xnat/commands.py
@@ -2,6 +2,7 @@
 
 """Commands defining actions that can be performed with copyxnat"""
 import abc
+import os
 
 from copyxnat.xnat.xnat_interface import XnatExperiment, XnatProject
 
@@ -17,28 +18,18 @@ def commands() -> dict:
 
 class Command:
     """Wraps a command function and its input variables"""
-    def __init__(self, command_inputs, initial_result=None):
-        self.initial_result = initial_result
+    def __init__(self, inputs, scope, initial_result=None):
+        self.scope = scope
         self.outputs = initial_result
-        self.inputs = {
-            'dst_xnat': command_inputs.dst_xnat,
-            'dst_project': command_inputs.dst_project,
-            'fix_scan_types': command_inputs.fix_scan_types,
-            'reporter': command_inputs.reporter
-        }
+        self.inputs = inputs
 
     @abc.abstractmethod
     def run(self, xnat_item, from_parent):
         """Function that will be run on each item in the XNAT server"""
 
-    def get_outputs(self):
-        return {'result': self.outputs, 'tostring': self.output_tostring}
-
-    def reset(self):
-        self.outputs = None
-
-    def output_tostring(self, result):
-        return "Output from {}: {}".format(self.NAME, result)
+    def print_results(self):
+        print("Result of running {} on {}:".format(self.NAME, self.scope))
+        print(self.outputs)
 
 
 class CommandInputs:
@@ -74,7 +65,10 @@ class ShowCommand(Command):
 
     def run(self, xnat_item, from_parent):  # pylint: disable=unused-argument
         """Command which displays contents of XNAT projects"""
-        print(xnat_item.user_visible_info())
+        if self.outputs is None:
+            self.outputs = ''
+        self.outputs += xnat_item.user_visible_info() + os.linesep
+        # print(xnat_item.user_visible_info())
         return CommandReturn()
 
 
@@ -87,14 +81,14 @@ class CopyCommand(Command):
         """Command which copies XNAT projects between servers"""
 
         # Override the project name
-        dst_name = self.inputs['dst_project'] if isinstance(xnat_item, XnatProject) \
+        dst_name = self.inputs.dst_project if isinstance(xnat_item, XnatProject) \
             else None
 
         copied_item = xnat_item.duplicate(
             destination_parent=from_parent,
-            fix_scan_types=self.inputs['fix_scan_types'],
+            fix_scan_types=self.inputs.fix_scan_types,
             dst_label=dst_name,
-            dry_run=self.inputs['reporter'].dry_run)
+            dry_run=self.inputs.reporter.dry_run)
         return CommandReturn(to_children=copied_item)
 
 
@@ -112,7 +106,7 @@ class CheckDatatypesCommand(Command):
         # On first run, set up the outputs
         if self.outputs is None:
             self.outputs = {
-                'datatypes_on_dest': self.inputs['dst_xnat'].datatypes(),
+                'datatypes_on_dest': self.inputs.dst_xnat.datatypes(),
                 'missing_session_datatypes': set(),
                 'ids_with_empty_datatypes': set(),
                 'required_experiment_datatypes': set()
@@ -124,15 +118,15 @@ class CheckDatatypesCommand(Command):
 
             if datatype not in self.outputs['datatypes_on_dest']:
                 self.outputs['missing_session_datatypes'].add(datatype)
-                self.inputs['reporter'].warning(
+                self.inputs.reporter.verbose_log(
                     'Session datatype {} needs to be added to '
                     'destination server. '.format(datatype))
 
             else:
-                self.inputs['reporter'].verbose_log('OK: session datatype {} is on '
+                self.inputs.reporter.verbose_log('OK: session datatype {} is on '
                                      'destination server.'.format(datatype))
         if datatype:
-            self.inputs['reporter'].verbose_log(
+            self.inputs.reporter.verbose_log(
                 'Known datatype for {} {} {}'.format(xnat_item._name,  # pylint: disable=protected-access
                                                      xnat_item.label,
                                                      xnat_item.full_name))
@@ -142,10 +136,26 @@ class CheckDatatypesCommand(Command):
                                         xnat_item.label,
                                         xnat_item.full_name)
 
-            self.inputs['reporter'].warning('Empty datatype for {}'.format(item_id))
+            self.inputs.reporter.verbose_log('Empty datatype for {}'.format(
+                item_id))
             self.outputs['ids_with_empty_datatypes'].add(item_id)
 
         return CommandReturn()
 
-    def output_tostring(self, result):
-        return "Output from {}: {}".format(self.NAME, result)
+    def print_results(self):
+        missing = self.outputs['missing_session_datatypes']
+        if missing:
+            print("Project {}: These datatypes need to be added to destination"
+                  " server:".format(self.scope))
+            for datatype in missing:
+                print('- {}'.format(datatype))
+        else:
+            print("Project {}: All datatypes present on destination server".
+                  format(self.scope))
+
+        empty = self.outputs['ids_with_empty_datatypes']
+        if empty:
+            print("Project {}: items with undefined datatype on source server:".
+                  format(self.scope))
+            for item in empty:
+                print('- {}'.format(item))
