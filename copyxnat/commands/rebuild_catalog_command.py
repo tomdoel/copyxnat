31882cc7fa5d744f2c38061a2c32f8b570fb88d7
Tom Doel <tomdoel@users.noreply.github.com>
2021-02-06 16:54:34 +0000

    Added command to rebuild catalog

---
+++ copyxnat/commands/rebuild_catalog_command.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+"""
+Command which rebuilds the catalog for sessions of specified projects
+"""
+
+from copyxnat.xnat.xnat_interface import XnatExperiment, XnatScan, XnatAssessor
+from copyxnat.xnat.commands import Command, CommandReturn
+
+
+class RebuildCatalogCommand(Command):
+    """Command which rebuilds the catalog for sessions of specified projects"""
+
+    NAME = 'Rebuild Catalog'
+    VERB = 'rebuild'
+    COMMAND_LINE = 'rebuild-catalog'
+    USE_DST_SERVER = False
+    CACHE_TYPE = 'cache'
+    HELP = 'Request a rebuild of the XNAT catalog to fix data issues'
+
+    def run(self, xnat_item,
+            from_parent):  # pylint: disable=unused-argument
+        xnat_item.rebuild_catalog()
+        return CommandReturn()
