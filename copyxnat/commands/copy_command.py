c8faebf9ffed9074e1885610921195f3ae28ed7d
Tom Doel <tomdoel@users.noreply.github.com>
2021-02-16 16:52:14 +0000

    Limit datatype checking to speed up processing

--- copyxnat/commands/copy_command.py
+++ copyxnat/commands/copy_command.py
@@ -43,26 +43,27 @@ class CopyCommand(Command):
                                    'destination project, not the Project Title '
                                    'or Running Title.'.format(dst_name))
 
-        datatype = xnat_item.datatype()
+        missing_session_type = False
+        if isinstance(xnat_item, XnatExperiment):
+            datatype = xnat_item.datatype()
+            missing_session_type = datatype not in self.dst_datatypes
 
-        missing_session_type = isinstance(xnat_item, XnatExperiment) and \
-                               datatype not in self.dst_datatypes
+            if missing_session_type:
+                item_id = '{} {}'.format(xnat_item._name, xnat_item.full_name)  # pylint: disable=protected-access
 
-        if missing_session_type:
-            item_id = '{} {}'.format(xnat_item._name, xnat_item.full_name)  # pylint: disable=protected-access
-
-            if self.inputs.app_settings.ignore_datatype_errors:
-                self.inputs.reporter.warning(
-                    'datatype {} is missing on the destination server but '
-                    '{} will be copied anyway because '
-                    '--ignore-datatype-errors was specified.'.format(
-                        datatype, item_id))
-            else:
-                self.inputs.reporter.warning(
-                    'Did not copy {} because datatype {} needs to be added '
-                    'to the destination server. To force copying of the data  '
-                    'use --ignore-datatype-errors '.format(item_id, datatype))
-                return
+                if self.inputs.app_settings.ignore_datatype_errors:
+                    self.inputs.reporter.warning(
+                        'datatype {} is missing on the destination server but '
+                        '{} will be copied anyway because '
+                        '--ignore-datatype-errors was specified.'.format(
+                            datatype, item_id))
+                else:
+                    self.inputs.reporter.warning(
+                        'Did not copy {} because datatype {} needs to be added '
+                        'to the destination server. To force copying of the '
+                        'data, use --ignore-datatype-errors '.
+                            format(item_id, datatype))
+                    return
 
         dst_copy = xnat_item.copy(
             destination_parent=from_parent,
