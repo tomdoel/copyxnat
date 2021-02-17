a2197ba1c37a4bdb5da1d1c0decce07a0686510e
Tom Doel <tomdoel@users.noreply.github.com>
2021-02-17 20:06:55 +0000

    Add rsync user parameter

--- copyxnat/xnat/xnat_interface.py
+++ copyxnat/xnat/xnat_interface.py
@@ -16,11 +16,13 @@ class XnatServerParams:
     """Encapsulates parameters used to access an XNAT server"""
 
     def __init__(self, host, user, pwd, insecure=False):
+    def __init__(self, host, user, pwd, rsync_user=None, insecure=False):
         if '://' not in host:
             host = 'https://' + host
         self.host = host
         self.user = user
         self.pwd = pwd
+        self.rsync_user = rsync_user
         self.insecure = insecure
 
 
