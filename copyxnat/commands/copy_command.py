7a9e7a458a08ac2ea4e8a308743549dd447412b3
Tom Doel <tomdoel@users.noreply.github.com>
2021-02-21 19:51:02 +0000

    Tidy up logic for skipping creation and recursion

--- copyxnat/commands/copy_command.py
+++ copyxnat/commands/copy_command.py
@@ -4,7 +4,7 @@
 from copyxnat.config.app_settings import TransferMode
 from copyxnat.xnat.xml_cleaner import XmlCleaner
 from copyxnat.xnat.xnat_interface import XnatProject, XnatExperiment, \
-    XnatFile, XnatResource, XnatOutResource, XnatInResource
+    XnatFile, XnatResource, XnatOutResource, XnatInResource, XnatSubject
 from copyxnat.xnat.commands import Command
 
 
@@ -54,10 +54,12 @@ class CopyCommand(Command):
         dst_copy = xnat_item.get_or_create_child(parent=from_parent,
                                                  label=label)
 
-        should_create, should_recurse = self._should_create(
-            dst_copy=dst_copy, xnat_item=xnat_item, label=label)
+        already_exists = dst_copy.exists_on_server()
 
         # Create the item on the destination server
+        should_create = self._should_create(
+            already_exists=already_exists, xnat_item=xnat_item, label=label)
+
         if should_create:
             dst_copy.create_from_source(src_item=xnat_item,
                                         xml_cleaner=self.xml_cleaner)
@@ -70,6 +72,9 @@ class CopyCommand(Command):
         self._rsync(dst_copy, xnat_item)
 
         # Recursion to the child items
+        should_recurse = self._should_recurse(
+            already_exists=already_exists, xnat_item=xnat_item, label=label)
+
         if should_recurse:
             self._recurse(xnat_item=xnat_item, to_children=dst_copy)
 
@@ -78,31 +83,50 @@ class CopyCommand(Command):
         if dst_copy:
             dst_copy.post_create()
 
-    def _should_create(self, dst_copy, xnat_item, label):
-        if dst_copy.exists_on_server():
-            if self.inputs.app_settings.overwrite_existing:
-                self.inputs.reporter.info(
-                    "Updating existing {} {}".format(xnat_item._name, label))  # pylint: disable=no-member, protected-access
-                write_dst = True
-            else:
-                self.inputs.reporter.info(
-                    "{} {} already exists on the destination "
-                    "server existing data will not be "
-                    "modified. Use the --overwrite_existing "
-                    "option to allow updating of existing "
-                    "data".format(xnat_item._name, label))  # pylint: disable=no-member, protected-access
-                write_dst = False
-            if self.inputs.app_settings.skip_existing:
-                self.inputs.reporter.info(
-                    "Will not process child items of existing {} {}".format(
-                        xnat_item._name, label))  # pylint: disable=no-member, protected-access
-                recurse = False
-            else:
-                recurse = True
-        else:
-            write_dst = True
-            recurse = True
-        return write_dst, recurse
+    def _should_create(self, already_exists, xnat_item, label):
+
+        # If item does not exist then create
+        if not already_exists:
+            return True
+
+        # If item exists, it's an experiment or descendent, and the
+        # skip-existing flag is set then do not create
+        if self.inputs.app_settings.skip_existing and \
+                not isinstance(xnat_item, (XnatProject, XnatSubject)):
+            self.inputs.reporter.info(
+                "Skipping existing {} {} and all of its child items".format(
+                    xnat_item._name, label))  # pylint: disable=no-member, protected-access
+            return False
+
+        # Do not overwrite unless overwrite flag is set
+        if not self.inputs.app_settings.overwrite_existing:
+            self.inputs.reporter.info(
+                "{} {} already exists on the destination "
+                "server. Existing data will not be "
+                "modified. Use the --overwrite_existing "
+                "option to allow updating of existing "
+                "data".format(xnat_item._name, label))  # pylint: disable=no-member, protected-access
+            return False
+
+        # Overwrite
+        self.inputs.reporter.info(
+            "Updating existing {} {}".format(xnat_item._name, label))  # pylint: disable=no-member, protected-access
+        return True
+
+    def _should_recurse(self, already_exists, xnat_item, label):
+
+        # If item already exists, is an Experiment or descendent and the
+        # skip-existing flag is set, then do not recurse.
+        # Do not display a message to the user as a message should already have
+        # been displayed during the _should_create() check
+        if already_exists and self.inputs.app_settings.skip_existing and \
+                not isinstance(xnat_item, (XnatProject, XnatSubject)):
+            self.inputs.reporter.log(
+                "Skipping children of existing {} {} becaue of skip-existing "
+                "flag".format(xnat_item._name, label))  # pylint: disable=no-member, protected-access
+            return False
+
+        return True
 
     def _check_session_types(self, xnat_item):
         """Check for missing session datatypes, Return True to continue copy"""
