fac7fd3a7a1aff664f8342c05bc4d3da1f481496
Tom Doel <tomdoel@users.noreply.github.com>
2021-03-06 15:13:08 +0000

    Extracted command line parsing into separate method

--- copyxnat/ui/command_line.py
+++ copyxnat/ui/command_line.py
@@ -15,10 +15,21 @@ from copyxnat.config.app_settings import AppSettings
 def run_command_line(args=None):
     """Entry point for copyxnat application"""
 
+    command, src_params, dst_params, project_list, app_settings = \
+        _parse_command_line(args)
+
+    run_command(command=command,
+                src_params=src_params,
+                dst_params=dst_params,
+                project_filter=project_list,
+                app_settings=app_settings
+                )
+
+
+def _parse_command_line(args):
     parser = argparse.ArgumentParser(
         description='CopyXnat: Utility for downloading or copying projects '
                     'between XNAT servers')
-
     version_string = get_version_string()
     friendly_version_string = version_string if version_string else 'unknown'
     parser.add_argument(
@@ -26,57 +37,45 @@ def run_command_line(args=None):
         action='version',
         version=friendly_version_string
     )
-
     subparsers = parser.add_subparsers(dest='command')
-
     for command in find_commands.commands():
         subparsers.add_parser(command.COMMAND_LINE, help=command.HELP)
-
     # Arguments common to all commands
     parser.add_argument("-y", "--dry_run",
                         action="store_true",
                         help="Do not modify the destination",
                         )
-
     parser.add_argument("-k", "--insecure",
                         action="store_true",
                         help="Do not enforce strict SSL certificate checks",
                         )
-
     parser.add_argument("-v", "--verbose",
                         action="store_true",
                         help="Show verbose output",
                         )
-
     parser.add_argument("-s", "--src-host", required=True,
                         help="Host name of the XNAT server containing the data"
                         )
-
     parser.add_argument("-u", "--src-user", required=True,
                         help="User name for accessing XNAT server containing "
                              "the data"
                         )
-
     parser.add_argument("-i", "--rsync-src-user",
                         default=None,
                         help="When using rsync transfer, ssh username for "
                              "source server (this is not the XNAT username)"
                         )
-
     parser.add_argument("-p", "--project", default=None,
                         help="Name of project containing the data"
                         )
-
     parser.add_argument("-c", "--cache-dir",
                         help="File path here local cache files are to be stored"
                         )
-
     parser.add_argument("-n", "--skip-existing",
                         action="store_true",
                         help="Skip session and all children if it already "
                              "exists on the destination server",
                         )
-
     parser.add_argument("-t", "--transfer-mode",
                         default='file',
                         choices=['file', 'zip', 'rsync', 'meta'],
@@ -92,7 +91,6 @@ def run_command_line(args=None):
                              " to have manually transferred the files over "
                              "first."
                         )
-
     for command in find_commands.commands():
         command_key = command.COMMAND_LINE
         sub_parser = subparsers.choices[command_key]
@@ -132,30 +130,22 @@ def run_command_line(args=None):
                                     help="Overwrite existing data on the"
                                          "destination server"
                                     )
-
     args = parser.parse_args(args)
-
     command = find_command(args.command)
-
     src_rsync = args.rsync_src_user if 'rsync_src_user' in args else None
-
     fix_scan_types = args.fix_scan_types if 'fix_scan_types' in args else False
     verbose = args.verbose if 'verbose' in args else False
     overwrite_existing = args.overwrite_existing if \
         'overwrite_existing' in args else False
     ignore_datatype_errors = args.ignore_datatype_errors if \
         'ignore_datatype_errors' in args else False
-
     transfer_mode = args.transfer_mode
-
     project_list = args.project.split(',') if 'project' in args and \
                                               args.project else None
-
     src_params = XnatServerParams(host=args.src_host,
                                   user=args.src_user,
                                   rsync_user=src_rsync,
                                   insecure=args.insecure)
-
     if command.USE_DST_SERVER:
         dst_host = args.dst_host if 'dst_host' in args else None
         dst_user = args.dst_user if 'dst_user' in args else None
@@ -178,9 +168,4 @@ def run_command_line(args=None):
         skip_existing=args.skip_existing
     )
 
-    run_command(command=command,
-                src_params=src_params,
-                dst_params=dst_params,
-                project_filter=project_list,
-                app_settings=app_settings
-                )
+    return command, src_params, dst_params, project_list, app_settings
