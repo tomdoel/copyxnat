7261883f5be04d0399abb00762bbdcc697ff723e
Tom Doel <tomdoel@users.noreply.github.com>
2021-03-06 14:05:51 +0000

    Tidy up of entry logic

--- copyxnat/__main__.py
+++ copyxnat/__main__.py
@@ -4,17 +4,18 @@
 
 import sys
 
-if __name__ == "__main__" and not __package__:
-    # To allow the package's main function to be executed without the -m switch,
-    # i.e. "python copyxnat", we have to explicitly set the
-    # module name and append the parent directory to the sys.path (see PEP 366)
-    from os import path
-    __package__ = "copyxnat"  # pylint: disable=redefined-builtin
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-    sys.path.append(path.dirname(path.dirname(__file__)))
-
-# pylint: disable=wrong-import-position
-from copyxnat.ui.command_line import run_command_line
-
 if __name__ == "__main__":
+
+    if not __package__:
+        # To allow the package's main function to be executed without the -m
+        # switch, i.e. "python copyxnat", we have to explicitly set the
+        # module name and append the parent directory to the sys.path
+        # (see PEP 366)
+        from os import path
+        __package__ = "copyxnat"  # pylint: disable=redefined-builtin
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+        sys.path.append(path.dirname(path.dirname(__file__)))
+
+    from copyxnat.ui.command_line import run_command_line
+
     sys.exit(run_command_line(sys.argv[1:]))
