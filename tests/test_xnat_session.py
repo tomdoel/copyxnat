df9c315eb4ba54b705e61c0cffcdca74209e3925
Tom Doel <tomdoel@users.noreply.github.com>
2021-07-04 07:48:45 +0100

    Added login/logout tests for XnatSession

--- tests/test_xnat_session.py
+++ tests/test_xnat_session.py
@@ -4,11 +4,141 @@
 
 import pytest
 import requests as requests
-from mockito import when, mock
+from mockito import when, mock, ANY, expect, verifyNoUnwantedInteractions
+from requests.auth import HTTPBasicAuth
 
 from copyxnat.config.server_params import XnatServerParams
 
-from copyxnat.xnat_backend.xnat_session import SessionId, RestWrapper, Utils
+from copyxnat.xnat_backend.xnat_session import SessionId, RestWrapper, \
+    XnatSession
+
+
+@pytest.mark.usefixtures('unstub')
+class TestXnatSession(object):
+
+    @pytest.mark.parametrize("host, url", [('http://testhost.local', 'http://testhost.local/data/JSESSION')])
+    @pytest.mark.parametrize("insecure, verify", [(True, False), (False, True)])
+    def test_login_logout(self, host, url, insecure, verify):
+        server_params = XnatServerParams(host=host,
+                                         user='fred',
+                                         insecure=insecure)
+        session = XnatSession(server_params)
+
+        cookie1 = 'first_session_id'
+        with self._mock_auth_login(url=url, success=True, verify=verify,
+                                   cookie=cookie1):
+            # Login
+            session.login()
+            verifyNoUnwantedInteractions()
+
+            # Second login should not trigger a rest call
+            session.login()
+            verifyNoUnwantedInteractions()
+
+        # Logout
+        with self._mock_auth_logout(url=url, success=True, verify=verify,
+                                    cookie=cookie1):
+            # Logout
+            session.logout()
+            verifyNoUnwantedInteractions()
+
+            # Second logout should not trigger a rest call
+            session.logout()
+            verifyNoUnwantedInteractions()
+
+        cookie2 = 'second_session_id'
+        with self._mock_auth_login(url=url, success=True, verify=verify,
+                                   cookie=cookie2):
+            # Login
+            session.login()
+            verifyNoUnwantedInteractions()
+
+            # Second login should not trigger auth call
+            session.login()
+            verifyNoUnwantedInteractions()
+
+        with self._mock_auth_logout(url=url, success=True, verify=verify,
+                                    cookie=cookie2):
+            # Logout
+            session.logout()
+            verifyNoUnwantedInteractions()
+
+            # Logged out; expect no rest calls on session deletion
+            session.__del__()
+            verifyNoUnwantedInteractions()
+
+    @pytest.mark.parametrize("host, url", [('http://testhost.local', 'http://testhost.local/data/JSESSION')])
+    def test_auto_logout(self, host, url):
+        server_params = XnatServerParams(host=host, user='fred')
+        session = XnatSession(server_params)
+
+        # Login
+        cookie = 'first_session_id'
+        with self._mock_auth_login(url=url, success=True, verify=True,
+                                   cookie=cookie):
+            session.login()
+            verifyNoUnwantedInteractions()
+
+        # Check auto logout on session deletion
+        with self._mock_auth_logout(url=url, success=True, verify=True,
+                                    cookie=cookie):
+            session.__del__()
+            verifyNoUnwantedInteractions()
+
+    @pytest.mark.parametrize("host, url", [('http://testhost.local', 'http://testhost.local/data/JSESSION')])
+    def test_no_auto_logout_if_already_logged_out(self, host, url):
+        server_params = XnatServerParams(host=host, user='fred')
+        session = XnatSession(server_params)
+        cookie = 'second_session_id'
+
+        with self._mock_auth_login(url=url, success=True, verify=True,
+                                   cookie=cookie):
+            # Login
+            session.login()
+            verifyNoUnwantedInteractions()
+
+        with self._mock_auth_logout(url=url, success=True, verify=True,
+                                    cookie=cookie):
+            # Logout
+            session.logout()
+            verifyNoUnwantedInteractions()
+
+            # Check that session deletion does not trigger another logout
+            session.__del__()
+            verifyNoUnwantedInteractions()
+
+    def _mock_auth_login(self, url, success, verify, cookie):
+        return self._mock_response(
+            method='post', url=url, success=success, verify=verify,
+            auth=ANY(HTTPBasicAuth), response_text=cookie,
+            expect_raise_for_status=True
+        )
+
+    def _mock_auth_logout(self, url, success, verify, cookie):
+        return self._mock_response(
+            method='delete', url=url, success=success, verify=verify,
+            headers={'Cookie': 'JSESSIONID='+cookie},
+            expect_raise_for_status=False
+        )
+
+    @staticmethod
+    def _mock_response(method, url, success, verify, headers=None,
+                       auth=None, response_text='OK',
+                       expect_raise_for_status=False):
+        mock_args = {
+            'status_code': 200,
+            'text': response_text,
+        }
+        if expect_raise_for_status:
+            mock_args['raise_for_status'] = lambda: None
+
+        auth_response = mock(mock_args, spec=requests.Response)
+
+        return expect(requests, times=1).request(
+            url=url, method=method, auth=auth, headers=headers,
+            params=None, data=None, verify=verify, stream=None
+        ).thenReturn(auth_response)
+
 
 
 @pytest.mark.usefixtures('unstub')
